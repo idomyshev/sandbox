3a170a9ddd3393d704cd4f2248c4bddbb334945b
iliajs <iadomyshev@gmail.com>
2024-04-25 02:51:36 +0200

    basic mock data

---
+++ XCode/AsyncAwait/AsyncAwait/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AsyncAwait
+//
+//  Created by Iliiah on 25.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
